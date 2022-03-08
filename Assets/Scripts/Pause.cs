a5decd06cf0c4f45673c5a30c3c7b90ec3c74871
WallyDough <Waldo.Rivera001@mymdc.net>
2022-03-08 10:26:51 -0500

    Implemented Pause Button
    Pause button now summons the pause menu.

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class Pause : MonoBehaviour
 {
     public GameObject PauseMenu; /* The GameObject itself is called "PauseMenu" */
+    public GameObject PauseButton;
 
     bool pauseOn;
 
     void Start()
     {
+        PauseButton.SetActive(true);
         PauseMenu.SetActive(false);
         pauseOn = false;
     }
@@ -21,12 +23,14 @@ public class Pause : MonoBehaviour
         {
             Time.timeScale = 0;
             PauseMenu.SetActive(true);
+            PauseButton.SetActive(false);
             pauseOn = true;
         }
         else if(Input.GetKeyDown(KeyCode.Escape) && pauseOn)
         {
             Time.timeScale = 1;
             PauseMenu.SetActive(false);
+            PauseButton.SetActive(true);
             pauseOn = false;
         }
     }
@@ -35,6 +39,7 @@ public class Pause : MonoBehaviour
     {
         Time.timeScale = 1;
         PauseMenu.SetActive(false);
+        PauseButton.SetActive(false);
         pauseOn = false;
     }
 
@@ -42,6 +47,14 @@ public class Pause : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quiting Application");
+    }
+
+    public void OpenMenu()
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+        PauseButton.SetActive(false);
+        pauseOn = true;
     }
 
 }
