83c7e3f9c0e6e057de4d80d78c356297e530967f
Nick Ruiz <ruizniko33@gmail.com>
2022-03-22 12:17:54 -0400

    Battle UI Changes
    For the Battle UI, I just made things a little bigger, as I was told that the buttons and robots are a bit small, I also added team names on top of the robots and repositioned the round sign to the top center of the screen. I also changed the watch sprites from the old one to new one.

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
@@ -45,8 +46,8 @@ public class Pause : MonoBehaviour
 
     public void Quit()
     {
-        Application.Quit();
-        Debug.Log("Quiting Application");
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Main Menu...");
     }
 
     public void OpenMenu()
